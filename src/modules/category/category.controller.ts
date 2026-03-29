059c02a856a197fe31cb06d691abb7e4f5e0b0d3
Habibov97 <najaff.habibov@gmail.com>
2026-03-29 21:58:27 +0400

    Added update and delete category features

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { AuthGuard } from 'src/guards/auth.guards';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('category')
 export class CategoryController {
@@ -18,7 +28,22 @@ export class CategoryController {
   @ApiBearerAuth()
   create(@Body() body: CreateCategoryDto) {
     const result = this.categoryService.create(body);
+    return result;
+  }
 
+  @Patch(':id')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  update(@Param('id') id: string, @Body() body: UpdateCategoryDto) {
+    const result = this.categoryService.update(id, body);
+    return result;
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  remove(@Param('id') id: string) {
+    const result = this.categoryService.remove(id);
     return result;
   }
 }
--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryEntity } from 'src/entities/Category.entity';
 import { Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import slugify from 'slugify';
 
 @Injectable()
 export class CategoryService {
@@ -20,5 +22,38 @@ export class CategoryService {
     await newCategory.save();
 
     return newCategory;
+  }
+
+  async update(id: string, params: UpdateCategoryDto) {
+    if (!params.slug && params.name) {
+      params.slug = slugify(params.name, { lower: true, strict: true });
+    }
+
+    const category = await this.categoryRepo.preload({
+      id: Number(id),
+      ...params,
+    });
+
+    if (!category) throw new NotFoundException('Category not found');
+
+    const updatedCategory = await this.categoryRepo.save(category);
+    return {
+      message: 'Category updated successfully',
+      category: updatedCategory,
+    };
+  }
+
+  async remove(id: string) {
+    const category = await this.categoryRepo.findOne({
+      where: { id: Number(id) },
+    });
+
+    if (!category) throw new NotFoundException('Category not found!');
+
+    await this.categoryRepo.remove(category);
+
+    return {
+      message: 'Category has been deleted successfully!',
+    };
   }
 }
